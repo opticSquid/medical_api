c746588c38ea111a933094586b34e42befbf2768
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-06-04 15:57:06 +0530

    Doctor Entity created

---
+++ src/main/java/com/sb/projects/java/spring/medical_api/entities/Doctor.java
@@ -0,0 +1,56 @@
+package com.sb.projects.java.spring.medical_api.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "doctors")
+public class Doctor {
+    @Id
+    private int id;
+    private String name;
+    private String email;
+    private String degree;
+    private String specialization;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+}
